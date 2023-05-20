188cec721703fb746f299cd5b6cff5b04eba3115
Henrique <henryqueoli@hotmail.com>
2023-05-20 16:47:02 -0300

    feat(mobile): new memory implementation

--- packages/mobile/app/new.tsx
+++ packages/mobile/app/new.tsx
@@ -1,16 +1,76 @@
-import { ScrollView, Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Icon from '@expo/vector-icons/Feather'
 
 import NlwLogo from '../src/assets/nlw-logo.svg'
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useState } from "react";
+import * as ImagePicker from 'expo-image-picker';
+import * as SecureStore from 'expo-secure-store';
+import { api } from "../src/lib/api";
 
 export default function NewMemory() {
 
+    const router = useRouter()
+
     const { bottom, top } = useSafeAreaInsets()
 
+    const [preview, setPreview] = useState<string | null>(null)
+    const [content, setContent] = useState('')
     const [isPublic, setIsPublic] = useState(false)
+
+    async function openMediaPicker(){
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            quality: 1,
+        });
+
+        if (result.assets[0]) {
+            setPreview(result.assets[0].uri);
+        }
+    }
+
+    async function handleUpload(){
+        const formData = new FormData()
+
+        formData.append('file', {
+            uri: preview,
+            name: 'image.jpg',
+            type: 'image/jpeg',
+        } as any)
+        
+        const response = await api.post('/upload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+
+        return response.data.fileURL
+    }
+
+
+    async function handleSubmit(){
+        const token = await SecureStore.getItemAsync('token')
+
+        let coverUrl = null
+
+        if (preview) {
+            coverUrl = await handleUpload()
+        }
+
+        await api.post('/memories', {
+            content,
+            isPublic,
+            coverUrl
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        router.push('/memories')
+    }
+
 
     return (
         <ScrollView        
@@ -46,13 +106,16 @@ export default function NewMemory() {
                     </Text>
                 </View>
 
-                <TouchableOpacity activeOpacity={0.7} className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-gray-800">
-                    <View className="flex-row items-center gap-2">
-                        <Icon name="image" size={24} color="#fff" />
-                        <Text className="font-bold text-sm text-gray-200">
-                            Adicionar foto ou video de capa
-                        </Text>
-                    </View>
+                <TouchableOpacity activeOpacity={0.7} className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-gray-800" onPress={openMediaPicker}>
+                    {preview ? (
+                        <Image source={{ uri: preview }} className="h-full w-full rounded-lg object-cover" />
+                    ) : (<View className="flex-row items-center gap-2">
+                            <Icon name="image" size={24} color="#fff" />
+                            <Text className="font-bold text-sm text-gray-200">
+                                Adicionar foto ou video de capa
+                            </Text>
+                        </View>
+                    )}
                 </TouchableOpacity>
 
                 <TextInput
@@ -60,12 +123,14 @@ export default function NewMemory() {
                     className="p-0 font-body text-lg text-gray-50"
                     placeholderTextColor='#56565a'
                     placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+                    value={content}
+                    onChangeText={setContent}
                 />
 
                 <TouchableOpacity
                     className='items-center self-end rounded-full bg-green-500 px-5 py-2 mb-4'
                     activeOpacity={0.7}
-                    onPress={() => {}}
+                    onPress={handleSubmit}
                 >
                     <Text className='font-alt text-sm uppercase text-black' >
                         Salvar
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -15,16 +15,15 @@ const PORT = 3333
 const HOST = '0.0.0.0'
 
 const app = fastify({
-  // logger: {
-  //   transport: {
-  //     target: 'pino-pretty',
-  //     options: {
-  //       translateTime: 'HH:MM:ss Z',
-  //       ignore: 'pid,hostname',
-  //     },
-  //   },
-  // },
-  
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },  
 })
 
 app.register(cors, { origin: ["*"]})
