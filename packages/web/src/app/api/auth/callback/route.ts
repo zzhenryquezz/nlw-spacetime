33e6a758deeaba10c53854630c850d5e29b7fb22
Henrique <henryqueoli@hotmail.com>
2023-05-20 13:32:31 -0300

    feat(web): layout & middleware

--- packages/web/src/app/api/auth/callback/route.ts
+++ packages/web/src/app/api/auth/callback/route.ts
@@ -5,12 +5,13 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
 
   const code = searchParams.get('code')
+  const redirectTo = request.cookies.get('redirectTo')?.value
 
   const response = await api.post('/register', { code })
 
   const { token } = response.data
 
-  const redirectURL = new URL('/', request.url)
+  const redirectURL = redirectTo ?? new URL('/', request.url)
 
   const expireInSeconds = 60 * 60 * 24 * 30
 
--- packages/web/src/components/Profile.tsx
+++ packages/web/src/components/Profile.tsx
@@ -16,7 +16,10 @@ export function Profile() {
 
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <a href="" className="hover:text-red-3000 block text-red-400">
+        <a
+          href="/api/auth/logout"
+          className="hover:text-red-3000 block text-red-400"
+        >
           Quero sair
         </a>
       </p>
